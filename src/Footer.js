0f5901ddbe61df46f0cc669001233569bc13ea2e
moustapha <modufall3@gmail.com>
2026-05-03 01:46:27 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,11 @@
 import './Footer.css';
 
-function Footer()
-{
-    return (
-        <footer className='footer'>
-            <p className='footer-titre'>© 2026 SénTransport - ESP/UCAD</p> 
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer className="footer">
+      &copy; 2026 SenTransport &mdash; ESP/UCAD
+    </footer>
+  );
 }
 
 export default Footer;
